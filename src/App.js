9297d9491f4a1e40ae136dde102069c7a878fbcb
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-30 22:33:24 -0500

    Install React Router | Change App Style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+// Libraries
+import { Routes, Route } from "react-router-dom";
+
+// Style
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <section className="header-section">
+          <img className="header-img" src='https://i.imgur.com/jXj1pih.png' alt="rok-university-logo" />
+          <h1>Welcome to RoK University</h1>
+        </section>
       </header>
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
